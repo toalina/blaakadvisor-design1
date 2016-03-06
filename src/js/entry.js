c498a8110ce1521e3ba0cef770161738c6055e2a
Alina To <alina.to@gmail.com>
2016-03-06 15:02:46 -0800

    Set up draggable dial in secure section

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,11 +1,10 @@
 // jquery knob
 require('./jquery.knob.min.js');
 require('./knob-scripts.js');
-// require('./excanvas.js');
 
 // grow money data calculator
-require('./blaakcalculator.js');
-require('./blaak-trendline.js');
+// require('./blaakcalculator.js');
+// require('./blaak-trendline.js');
 
 // input error
-require('./mailchimp.js');
+// require('./mailchimp.js');
