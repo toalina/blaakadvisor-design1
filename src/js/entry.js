fa85e4543a0c38fe871e757d8e4ea90cebe2737b
Alina To <alina.to@gmail.com>
2016-03-06 14:23:19 -0800

    Complete email input error alert

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,3 +1,11 @@
+// jquery knob
 require('./jquery.knob.min.js');
-require('./scripts.js');
-require('./excanvas.js');
+require('./knob-scripts.js');
+// require('./excanvas.js');
+
+// grow money data calculator
+require('./blaakcalculator.js');
+require('./blaak-trendline.js');
+
+// input error
+require('./mailchimp.js');
