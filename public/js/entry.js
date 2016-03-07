5d58179c0b38e491248f1252e8f08bcdec719907
Alina To <alina.to@gmail.com>
2016-03-07 12:14:06 -0800

    Modify gitignore

---
+++ public/js/entry.js
@@ -0,0 +1,11 @@
+// // jquery knob
+require('./jquery.knob.min.js');
+require('./knob-scripts.js');
+
+// grow money data calculator
+require('./blaak-trendline.js');
+require('./blaak-calculator.js');
+
+
+// input error
+require('./mailchimp.js');
