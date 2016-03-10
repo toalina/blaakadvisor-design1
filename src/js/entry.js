b5a8cc6aa2a20099804afa3c4f6716b8e3ec2992
Adam Bickford <xjunkyardx@gmail.com>
2016-03-09 21:34:58 -0800

    use never-merged knob pr to add knob to knob dial and get styles much much closer to the design

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,11 +1,12 @@
 // jquery knob
-// require('./jquery.knob.min.js');
+require('./jquery.knob.js');
 // // require('./knob-scripts.js');
 
 // // grow money data calculator
 // require('./blaak-trendline.js');
 // require('./blaak-calculator.js');
 
+require('./blaak-slider.js');
 
 // // input error
-// require('./mailchimp.js');
+require('./mailchimp.js');
